c9b6c8e88d540e7d5a41757b53f531ab7d78e256
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:09:22 +0100

    changed: var names in push and handled betty

--- push.c
+++ push.c
@@ -2,10 +2,10 @@
 /**
  * f_push - add node to the stack
  * @front: stack front
- * @num_popped: line_number
+ * @numPop: line_number
  * Return: no return
 */
-void f_push(stack_t **front, unsigned int num_popped)
+void f_push(stack_t **front, unsigned int numPop)
 {
 	int n, j = 0, flag = 0;
 
@@ -18,15 +18,15 @@ void f_push(stack_t **front, unsigned int num_popped)
 			if (custom_struct.argument[j] > 57 || custom_struct.argument[j] < 48)
 				flag = 1; }
 		if (flag == 1)
-		{ fprintf(stderr, "L%d: usage: push integer\n", num_popped);
-			fclose(custom_struct.file_pointer);
-			free(custom_struct.line_content);
+		{ fprintf(stderr, "L%d: usage: push integer\n", numPop);
+			fclose(custom_struct.file_ptr);
+			free(custom_struct.line_cont);
 			free_stack(*front);
 			exit(EXIT_FAILURE); }}
 	else
-	{ fprintf(stderr, "L%d: usage: push integer\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+	{ fprintf(stderr, "L%d: usage: push integer\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE); }
 	n = atoi(custom_struct.argument);
