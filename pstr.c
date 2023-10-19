a9eab8a06423cd10ab4b0c926550c5b7fbe500cf
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 15:28:38 +0100

    change head to front in pstr

--- pstr.c
+++ pstr.c
@@ -2,16 +2,16 @@
 /**
  * f_pstr - prints the string starting at the top of the stack,
  * followed by a new
- * @head: stack head
+ * @front: stack front
  * @counter: line_number
  * Return: no return
 */
-void f_pstr(stack_t **head, unsigned int counter)
+void f_pstr(stack_t **front, unsigned int counter)
 {
 	stack_t *h;
 	(void)counter;
 
-	h = *head;
+	h = *front;
 	while (h)
 	{
 		if (h->n > 127 || h->n <= 0)
