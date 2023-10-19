de42e7bb809e156008b32f2047ba18efea7a0220
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:36:21 +0100

    feat: change usr def fncs in - (pint)

--- pint.c
+++ pint.c
@@ -1,18 +1,18 @@
 #include "monty.h"
 /**
- * f_pint - prints the top
+ * monty_nodepint - prints the top
  * @front: stack front
  * @numPop: line_number
  * Return: no return
 */
-void f_pint(stack_t **front, unsigned int numPop)
+void monty_nodepint(stack_t **front, unsigned int numPop)
 {
 	if (*front == NULL)
 	{
 		fprintf(stderr, "L%u: can't pint, stack empty\n", numPop);
 		fclose(custom_struct.file_ptr);
 		free(custom_struct.line_cont);
-		free_stack(*front);
+		freeStacks(*front);
 		exit(EXIT_FAILURE);
 	}
 	printf("%d\n", (*front)->n);
