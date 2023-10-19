4fd2883b6cbc84bb14a723f3f01c237af82ecf08
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 16:34:43 +0100

    modified "h" to "n_clone" in - (pchar)

--- pchar.c
+++ pchar.c
@@ -8,10 +8,10 @@
 */
 void f_pchar(stack_t **front, unsigned int num_popped)
 {
-	stack_t *h;
+	stack_t *n_clone;
 
-	h = *front;
-	if (!h)
+	n_clone = *front;
+	if (!n_clone)
 	{
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", num_popped);
 		fclose(custom_struct.file_pointer);
@@ -19,7 +19,7 @@ void f_pchar(stack_t **front, unsigned int num_popped)
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	if (h->n > 127 || h->n < 0)
+	if (n_clone->n > 127 || n_clone->n < 0)
 	{
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", num_popped);
 		fclose(custom_struct.file_pointer);
@@ -27,5 +27,5 @@ void f_pchar(stack_t **front, unsigned int num_popped)
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	printf("%c\n", h->n);
+	printf("%c\n", n_clone->n);
 }
