ab096c2d3b62e84564047f0a0acbf042d1b87211
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 15:29:12 +0100

    change head to front in swap

--- swap.c
+++ swap.c
@@ -1,16 +1,16 @@
 #include "monty.h"
 /**
  * f_swap - adds the top two elements of the stack.
- * @head: stack head
+ * @front: stack front
  * @counter: line_number
  * Return: no return
 */
-void f_swap(stack_t **head, unsigned int counter)
+void f_swap(stack_t **front, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
 
-	h = *head;
+	h = *front;
 	while (h)
 	{
 		h = h->next;
@@ -21,10 +21,10 @@ void f_swap(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
 		fclose(custom_struct.file_pointer);
 		free(custom_struct.line_content);
-		free_stack(*head);
+		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
+	h = *front;
 	aux = h->n;
 	h->n = h->next->n;
 	h->next->n = aux;
