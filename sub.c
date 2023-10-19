f1bf309799712f1a9de394882ef8916dfbd29377
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:09:44 +0100

    changed: var names in sub and handled betty

--- sub.c
+++ sub.c
@@ -2,28 +2,28 @@
 /**
   *f_sub- sustration
   *@front: stack front
-  *@num_popped: line_number
+  *@numPop: line_number
   *Return: no return
  */
-void f_sub(stack_t **front, unsigned int num_popped)
+void f_sub(stack_t **front, unsigned int numPop)
 {
-	stack_t *aux;
+	stack_t *_get_n;
 	int sus, nodes;
 
-	aux = *front;
-	for (nodes = 0; aux != NULL; nodes++)
-		aux = aux->next;
+	_get_n = *front;
+	for (nodes = 0; _get_n != NULL; nodes++)
+		_get_n = _get_n->next;
 	if (nodes < 2)
 	{
-		fprintf(stderr, "L%d: can't sub, stack too short\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+		fprintf(stderr, "L%d: can't sub, stack too short\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	aux = *front;
-	sus = aux->next->n - aux->n;
-	aux->next->n = sus;
-	*front = aux->next;
-	free(aux);
+	_get_n = *front;
+	sus = _get_n->next->n - _get_n->n;
+	_get_n->next->n = sus;
+	*front = _get_n->next;
+	free(_get_n);
 }
