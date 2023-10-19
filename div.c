f3087208ecdbddcbfe1ebe9cd442f26a05f1ac9d
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 15:27:56 +0100

    change head to front in div

--- div.c
+++ div.c
@@ -1,16 +1,16 @@
 #include "monty.h"
 /**
  * f_div - divides the top two elements of the stack.
- * @head: stack head
+ * @front: stack front
  * @counter: line_number
  * Return: no return
 */
-void f_div(stack_t **head, unsigned int counter)
+void f_div(stack_t **front, unsigned int counter)
 {
 	stack_t *h;
 	int len = 0, aux;
 
-	h = *head;
+	h = *front;
 	while (h)
 	{
 		h = h->next;
@@ -21,20 +21,20 @@ void f_div(stack_t **head, unsigned int counter)
 		fprintf(stderr, "L%d: can't div, stack too short\n", counter);
 		fclose(custom_struct.file_pointer);
 		free(custom_struct.line_content);
-		free_stack(*head);
+		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	h = *head;
+	h = *front;
 	if (h->n == 0)
 	{
 		fprintf(stderr, "L%d: division by zero\n", counter);
 		fclose(custom_struct.file_pointer);
 		free(custom_struct.line_content);
-		free_stack(*head);
+		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
 	aux = h->next->n / h->n;
 	h->next->n = aux;
-	*head = h->next;
+	*front = h->next;
 	free(h);
 }
