72a41e24ac06d3a519816ec13f05fc2eb79eb0e0
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:09:00 +0100

    changed: var names in nop and handled betty

--- nop.c
+++ nop.c
@@ -2,11 +2,11 @@
 /**
   *f_nop- nothing
   *@front: stack front
-  *@num_popped: line_number
+  *@numPop: line_number
   *Return: no return
  */
-void f_nop(stack_t **front, unsigned int num_popped)
+void f_nop(stack_t **front, unsigned int numPop)
 {
-	(void) num_popped;
+	(void) numPop;
 	(void) front;
 }
