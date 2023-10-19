22f69778bf5e3a78a14574c5286aac62f53d464e
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:36:18 +0100

    feat: change usr def fncs in - (pchar)

--- pchar.c
+++ pchar.c
@@ -1,12 +1,12 @@
 #include "monty.h"
 /**
- * f_pchar - prints the char at the top of the stack,
+ * monty_nodepchar - prints the char at the top of the stack,
  * followed by a new line
  * @front: stack front
  * @numPop: line_number
  * Return: no return
 */
-void f_pchar(stack_t **front, unsigned int numPop)
+void monty_nodepchar(stack_t **front, unsigned int numPop)
 {
 	stack_t *n_clone;
 
@@ -16,7 +16,7 @@ void f_pchar(stack_t **front, unsigned int numPop)
 		fprintf(stderr, "L%d: can't pchar, stack empty\n", numPop);
 		fclose(custom_struct.file_ptr);
 		free(custom_struct.line_cont);
-		free_stack(*front);
+		freeStacks(*front);
 		exit(EXIT_FAILURE);
 	}
 	if (n_clone->n > 127 || n_clone->n < 0)
@@ -24,7 +24,7 @@ void f_pchar(stack_t **front, unsigned int numPop)
 		fprintf(stderr, "L%d: can't pchar, value out of range\n", numPop);
 		fclose(custom_struct.file_ptr);
 		free(custom_struct.line_cont);
-		free_stack(*front);
+		freeStacks(*front);
 		exit(EXIT_FAILURE);
 	}
 	printf("%c\n", n_clone->n);
