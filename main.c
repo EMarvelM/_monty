edd5061a5ec30c89bf2ba5080e350eff1e147e7f
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:08:39 +0100

    changed: var names in main and handled betty

--- main.c
+++ main.c
@@ -3,43 +3,43 @@ custom_struct_t custom_struct = {NULL, NULL, NULL, 0};
 /**
 * main - monty code interpreter
 * @argc: num of arguments
-* @argv: monty file_pointer location
+* @argv: monty file_ptr location
 * Return: 0 on success
 */
 int main(int argc, char *argv[])
 {
-	char *line_content;
-	FILE *file_pointer;
-	size_t size = 0;
-	ssize_t read_line = 1;
 	stack_t *stack = NULL;
-	unsigned int num_popped = 0;
+	unsigned int numPop = 0;
+	ssize_t read_line = 1;
+	char *line_cont;
+	FILE *file_ptr;
+	size_t sz = 0;
 
 	if (argc != 2)
 	{
-		fprintf(stderr, "USAGE: monty file_pointer\n");
+		fprintf(stderr, "USAGE: monty file_ptr\n");
 		exit(EXIT_FAILURE);
 	}
-	file_pointer = fopen(argv[1], "r");
-	custom_struct.file_pointer = file_pointer;
-	if (!file_pointer)
+	file_ptr = fopen(argv[1], "r");
+	custom_struct.file_ptr = file_ptr;
+	if (!file_ptr)
 	{
-		fprintf(stderr, "Error: Can't open file_pointer %s\n", argv[1]);
+		fprintf(stderr, "Error: Can't open file_ptr %s\n", argv[1]);
 		exit(EXIT_FAILURE);
 	}
 	while (read_line > 0)
 	{
-		line_content = NULL;
-		read_line = getline(&line_content, &size, file_pointer);
-		custom_struct.line_content = line_content;
-		num_popped++;
+		line_cont = NULL;
+		read_line = getline(&line_cont, &sz, file_ptr);
+		custom_struct.line_cont = line_cont;
+		numPop++;
 		if (read_line > 0)
 		{
-			execute(line_content, &stack, num_popped, file_pointer);
+			exec(line_cont, &stack, numPop, file_ptr);
 		}
-		free(line_content);
+		free(line_cont);
 	}
 	free_stack(stack);
-	fclose(file_pointer);
+	fclose(file_ptr);
 return (0);
 }
