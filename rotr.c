c9c535406925e80b6b67a4cc5c39fd7d978c0084
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 16:05:25 +0100

    changed: counter to num_popped in - rotr

--- rotr.c
+++ rotr.c
@@ -2,10 +2,10 @@
 /**
   *f_rotr- rotates the stack to the bottom
   *@front: stack front
-  *@counter: line_number
+  *@num_popped: line_number
   *Return: no return
  */
-void f_rotr(stack_t **front, __attribute__((unused)) unsigned int counter)
+void f_rotr(stack_t **front, __attribute__((unused)) unsigned int num_popped)
 {
 	stack_t *copy;
 
