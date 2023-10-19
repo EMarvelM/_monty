a6b74f5c6cfc87639468c878ba3eda8913e4bc0e
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:08:28 +0100

    changed: var names in div and handled betty

--- div.c
+++ div.c
@@ -2,39 +2,39 @@
 /**
  * f_div - divides the top two elements of the stack.
  * @front: stack front
- * @num_popped: line_number
+ * @numPop: line_number
  * Return: no return
 */
-void f_div(stack_t **front, unsigned int num_popped)
+void f_div(stack_t **front, unsigned int numPop)
 {
 	stack_t *n_clone;
-	int len = 0, aux;
+	int length = 0, _get_n;
 
 	n_clone = *front;
 	while (n_clone)
 	{
 		n_clone = n_clone->next;
-		len++;
+		length++;
 	}
-	if (len < 2)
+	if (length < 2)
 	{
-		fprintf(stderr, "L%d: can't div, stack too short\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+		fprintf(stderr, "L%d: can't div, stack too short\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
 	n_clone = *front;
 	if (n_clone->n == 0)
 	{
-		fprintf(stderr, "L%d: division by zero\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+		fprintf(stderr, "L%d: division by zero\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	aux = n_clone->next->n / n_clone->n;
-	n_clone->next->n = aux;
+	_get_n = n_clone->next->n / n_clone->n;
+	n_clone->next->n = _get_n;
 	*front = n_clone->next;
 	free(n_clone);
 }
