8b5779020cb4ad766f97b08a11b02b3fc37fd629
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:09:48 +0100

    changed: var names in swap and handled betty

--- swap.c
+++ swap.c
@@ -2,30 +2,30 @@
 /**
  * f_swap - adds the top two elements of the stack.
  * @front: stack front
- * @num_popped: line_number
+ * @numPop: line_number
  * Return: no return
 */
-void f_swap(stack_t **front, unsigned int num_popped)
+void f_swap(stack_t **front, unsigned int numPop)
 {
 	stack_t *n_clone;
-	int len = 0, aux;
+	int length = 0, _get_n;
 
 	n_clone = *front;
 	while (n_clone)
 	{
 		n_clone = n_clone->next;
-		len++;
+		length++;
 	}
-	if (len < 2)
+	if (length < 2)
 	{
-		fprintf(stderr, "L%d: can't swap, stack too short\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+		fprintf(stderr, "L%d: can't swap, stack too short\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
 	n_clone = *front;
-	aux = n_clone->n;
+	_get_n = n_clone->n;
 	n_clone->n = n_clone->next->n;
-	n_clone->next->n = aux;
+	n_clone->next->n = _get_n;
 }
