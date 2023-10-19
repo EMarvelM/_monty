366c592cf6d71fdf07db5304f9d4c3ef94766bc6
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:08:31 +0100

    changed: var names in execute and handled betty

--- execute.c
+++ execute.c
@@ -1,13 +1,13 @@
 #include "monty.h"
 /**
-* execute - executes the opcode
+* exec - executes the opcode
 * @stack: front linked list - stack
-* @num_popped: line_counter
-* @file_pointer: poiner to monty file_pointer
-* @line_content: line line_content
+* @numPop: line_counter
+* @file_ptr: poiner to monty file_ptr
+* @line_cont: line line_cont
 * Return: no return
 */
-int execute(char *line_content, stack_t **stack, unsigned int num_popped, FILE *file_pointer)
+int exec(char *line_cont, stack_t **stack, unsigned int numPop, FILE *file_ptr)
 {
 	instruction_t opst[] = {
 				{"push", f_push}, {"pall", f_pall}, {"pint", f_pint},
@@ -30,22 +30,22 @@ int execute(char *line_content, stack_t **stack, unsigned int num_popped, FILE *
 	unsigned int i = 0;
 	char *op;
 
-	op = strtok(line_content, " \n\t");
+	op = strtok(line_cont, " \n\t");
 	if (op && op[0] == '#')
 		return (0);
 	custom_struct.argument = strtok(NULL, " \n\t");
 	while (opst[i].opcode && op)
 	{
 		if (strcmp(op, opst[i].opcode) == 0)
-		{	opst[i].f(stack, num_popped);
+		{	opst[i].f(stack, numPop);
 			return (0);
 		}
 		i++;
 	}
 	if (op && opst[i].opcode == NULL)
-	{ fprintf(stderr, "L%d: unknown instruction %s\n", num_popped, op);
-		fclose(file_pointer);
-		free(line_content);
+	{ fprintf(stderr, "L%d: unknown instruction %s\n", numPop, op);
+		fclose(file_ptr);
+		free(line_cont);
 		free_stack(*stack);
 		exit(EXIT_FAILURE); }
 	return (1);
