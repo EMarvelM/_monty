67a759ce97198c127cdac36d7d1b5c62929f25a1
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:08:20 +0100

    changed: var names in add and handled betty

--- add.c
+++ add.c
@@ -2,31 +2,31 @@
 /**
  * f_add - adds the top two elements of the stack.
  * @front: stack front
- * @num_popped: line_number
+ * @numPop: line_number
  * Return: no return
 */
-void f_add(stack_t **front, unsigned int num_popped)
+void f_add(stack_t **front, unsigned int numPop)
 {
 	stack_t *n_clone;
-	int len = 0, aux;
+	int length = 0, _get_n;
 
 	n_clone = *front;
 	while (n_clone)
 	{
 		n_clone = n_clone->next;
-		len++;
+		length++;
 	}
-	if (len < 2)
+	if (length < 2)
 	{
-		fprintf(stderr, "L%d: can't add, stack too short\n", num_popped);
-		fclose(custom_struct.file_pointer);
-		free(custom_struct.line_content);
+		fprintf(stderr, "L%d: can't add, stack too short\n", numPop);
+		fclose(custom_struct.file_ptr);
+		free(custom_struct.line_cont);
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
 	n_clone = *front;
-	aux = n_clone->n + n_clone->next->n;
-	n_clone->next->n = aux;
+	_get_n = n_clone->n + n_clone->next->n;
+	n_clone->next->n = _get_n;
 	*front = n_clone->next;
 	free(n_clone);
 }
