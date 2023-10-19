aa00e4b64d81686dec4f25adfe420f152024d2f0
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 16:35:57 +0100

    modified "h" to "n_clone" in - (swap)

--- swap.c
+++ swap.c
@@ -7,13 +7,13 @@
 */
 void f_swap(stack_t **front, unsigned int num_popped)
 {
-	stack_t *h;
+	stack_t *n_clone;
 	int len = 0, aux;
 
-	h = *front;
-	while (h)
+	n_clone = *front;
+	while (n_clone)
 	{
-		h = h->next;
+		n_clone = n_clone->next;
 		len++;
 	}
 	if (len < 2)
@@ -24,8 +24,8 @@ void f_swap(stack_t **front, unsigned int num_popped)
 		free_stack(*front);
 		exit(EXIT_FAILURE);
 	}
-	h = *front;
-	aux = h->n;
-	h->n = h->next->n;
-	h->next->n = aux;
+	n_clone = *front;
+	aux = n_clone->n;
+	n_clone->n = n_clone->next->n;
+	n_clone->next->n = aux;
 }
