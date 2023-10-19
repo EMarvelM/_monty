8c45e1d0972e55f794a1645b641fb01185d16eab
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 16:05:28 +0100

    changed: counter to num_popped in - stack

--- stack.c
+++ stack.c
@@ -2,12 +2,12 @@
 /**
  * f_stack - prints the top
  * @front: stack front
- * @counter: line_number
+ * @num_popped: line_number
  * Return: no return
 */
-void f_stack(stack_t **front, unsigned int counter)
+void f_stack(stack_t **front, unsigned int num_popped)
 {
 	(void)front;
-	(void)counter;
+	(void)num_popped;
 	custom_struct.stack_queue_flag = 0;
 }
