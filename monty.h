11a956c6b42da9907c20aef168b38bd556d6f138
EMarvelM <marvelousegbe19@gmail.com>
2023-10-19 18:08:52 +0100

    changed: var names in monty and handled betty

--- monty.h
+++ monty.h
@@ -29,8 +29,8 @@ typedef struct stack_s
 /**
  * struct custom_struct_s - Custom structure to store variables
  * @argument: value
- * @file_pointer: pointer to a file (monty file), actual usuage - test
- * @line_content: Content of a file (read lines)
+ * @file_ptr: pointer to a file (monty file), actual usuage - test
+ * @line_cont: Content of a file (read lines)
  * @stack_queue_flag: Flag to switch between stack and queue
  *
  * Description: This structure holds various values used in the program.
@@ -38,8 +38,8 @@ typedef struct stack_s
 typedef struct custom_struct_s
 {
 	char *argument;
-	FILE *file_pointer;
-	char *line_content;
+	FILE *file_ptr;
+	char *line_cont;
 	int stack_queue_flag;
 }  custom_struct_t;
 /*make custom datas available all the file*/
@@ -62,32 +62,31 @@ typedef struct instruction_s
 /*fncs not def and not used - E*/
 /*char *cust_realloc(char *block_ptr, unsigned int old_sz,*/
 /*unsigned int new_sz);*/
-/*ssize_t read_stdin(char **line_ptr, int file_pointer);*/
-/*char  *flush_line(char *line_content);*/
+/*ssize_t read_stdin(char **line_ptr, int file_ptr);*/
+/*char  *flush_line(char *line_cont);*/
 
 /*usr def fncs*/
 void f_push(stack_t **front, unsigned int num);
 void f_pall(stack_t **front, unsigned int num);
 void f_pint(stack_t **front, unsigned int num);
-int execute(char *line_content, stack_t **front, unsigned int num_popped,
-FILE *file_pointer);
+int exec(char *, stack_t **, unsigned int numPop, FILE *);
 void free_stack(stack_t *front);
-void f_pop(stack_t **front, unsigned int num_popped);
-void f_swap(stack_t **front, unsigned int num_popped);
-void f_add(stack_t **front, unsigned int num_popped);
-void f_nop(stack_t **front, unsigned int num_popped);
-void f_sub(stack_t **front, unsigned int num_popped);
-void f_div(stack_t **front, unsigned int num_popped);
-void f_mul(stack_t **front, unsigned int num_popped);
-void f_mod(stack_t **front, unsigned int num_popped);
-void f_pchar(stack_t **front, unsigned int num_popped);
-void f_pstr(stack_t **front, unsigned int num_popped);
-void f_rotl(stack_t **front, unsigned int num_popped);
-void f_rotr(stack_t **front, __attribute__((unused)) unsigned int num_popped);
+void f_pop(stack_t **front, unsigned int numPop);
+void f_swap(stack_t **front, unsigned int numPop);
+void f_add(stack_t **front, unsigned int numPop);
+void f_nop(stack_t **front, unsigned int numPop);
+void f_sub(stack_t **front, unsigned int numPop);
+void f_div(stack_t **front, unsigned int numPop);
+void f_mul(stack_t **front, unsigned int numPop);
+void f_mod(stack_t **front, unsigned int numPop);
+void f_pchar(stack_t **front, unsigned int numPop);
+void f_pstr(stack_t **front, unsigned int numPop);
+void f_rotl(stack_t **front, unsigned int numPop);
+void f_rotr(stack_t **front, __attribute__((unused)) unsigned int numPop);
 void addnode(stack_t **front, int n);
 void addqueue(stack_t **front, int n);
-void f_queue(stack_t **front, unsigned int num_popped);
-void f_stack(stack_t **front, unsigned int num_popped);
+void f_queue(stack_t **front, unsigned int numPop);
+void f_stack(stack_t **front, unsigned int numPop);
 
 
 #endif /*MONTY_H*/
